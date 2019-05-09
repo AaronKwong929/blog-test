120a9a0deccf8ec3b542d7bfc1ce20374b86ef15
AaronKwong929 <AaronKwong929@gmail.com>
2019-05-09 21:16:13 +0800

    add signup

--- controllers/index.js
+++ controllers/index.js
@@ -7,22 +7,28 @@ var fn_index = async (ctx, next) => {
 };
 
 var fn_signIn = async (ctx, next) => {
-    var name = ctx.request.body.name,
-        password = ctx.request.body.password;
-    if (name === 'admin' && password === '123') {
-        ctx.render('signin-ok.html', {
-            title: 'Sign In OK',
-            name: 'Aaron'
+    if (ctx.method === 'GET') {
+        ctx.render('signin.html',{
+            title: 'Sign In',
         });
-    } else {
-        ctx.render('signin-failed.html', {
-            title: 'Sign In Failed.'
-        });
+    } else if (ctx.method === 'POST') {
+        var name = ctx.request.body.name,
+            password = ctx.request.body.password;
+        if (name === 'admin' && password === '123') {
+            ctx.render('signin-ok.html', {
+                title: 'Sign In OK',
+                name: name,
+            });
+        } else {
+            ctx.render('signin-failed.html', {
+                title: 'Sign In Failed.'
+            });
+        }
     }
 };
 
 var fn_catalog = async (ctx, next) => {
-    var articless = articles.getArticles();
+    var articless = articles.loadArticles();
     ctx.render('catalog.html', {
         title: 'Articles Catalog',
         articless
@@ -45,10 +51,8 @@ var fn_new = async (ctx, next) => {
 
 var fn_new_submit = async (ctx, next) => {
     var title = ctx.request.body.title,
-        content = ctx.request.body.content,
-        time = new Date().toLocaleString();
-    console.log(`${title}  ${content}  ${time}`);
-    articles.addArticle(title, content, time);
+        content = ctx.request.body.content;
+    articles.addArticle(title, content);
     ctx.redirect(`/articles/${title}`);
 };
 
@@ -70,16 +74,39 @@ var fn_edit = async (ctx, next) => {
         });
     } else if (ctx.method === 'POST') {
         var title = ctx.request.body.title,
-            content = ctx.request.body.content,
-            time = new Date().toLocaleString();
-        console.log(`${title}  ${content} ${time}`);
-        articles.editArticle(title, content, time);
-        ctx.redirect('/articles');
+            content = ctx.request.body.content;
+        articles.editArticle(title, content);
+        ctx.redirect(`/articles/${title}`);
+    }
+};
+
+var fn_about = (ctx, next) => {
+    ctx.render('about.html', {
+        title: 'About',
+    });
+};
+
+var fn_signUp = (ctx, next) => {
+    if (ctx.method === 'GET') {
+        ctx.render('signup.html',{
+
+        });
+    } else if (ctx.method === 'POST') {
+        if (true) {
+            ctx.render('signup-ok.html', {
+                title: 'Successful',
+            });
+        } else if (false) {
+            ctx.render('signup-failed.html'), {
+                title: 'Failed',
+            }
+        }
     }
 };
 
 module.exports = {
     'GET /': fn_index,
+    'GET /signin': fn_signIn,
     'POST /signin': fn_signIn,
     'GET /articles': fn_catalog,
     'GET /articles/:title': fn_article,
@@ -87,5 +114,8 @@ module.exports = {
     'POST /new': fn_new_submit,
     'GET /articles/:title/delete': fn_delete,
     'GET /articles/:title/edit': fn_edit,
-    'POST /articles/:title/edit': fn_edit
+    'POST /articles/:title/edit': fn_edit,
+    'GET /about': fn_about,
+    'GET /signup': fn_signUp,
+    'POST /signup': fn_signUp,
 };
