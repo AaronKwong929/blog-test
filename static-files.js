00a92d07be919a15e9cc2e582c6bdebea1f90b93
AaronKwong929 <AaronKwong929@gmail.com>
2019-05-19 11:23:47 +0800

    mime相关注释

--- static-files.js
+++ static-files.js
@@ -9,11 +9,12 @@ var staticFiles = function(url, dir) {
         let rpath = ctx.request.path;
         if (rpath.startsWith(url)){
             // 获取文件完整路径
-            let fp = path.join(dir, rpath.substring(url.length));
+            let fp = path.join(dir, rpath.substring(url.length));  // 从/static/之后开始截取
             // 判断文件是否存在
             if (await fs.existsSync(fp)) {
                 //查找文件的mime
                 ctx.response.type = mime.getType(rpath);
+                // mime是查找类型，返回的是'text/css'
                 // 读取文件内容 赋值给response.body：
                 ctx.response.body = await fs.readFile(fp);
             } else {
@@ -28,3 +29,5 @@ var staticFiles = function(url, dir) {
 }
 
 module.exports = staticFiles;
+
+// 使用了mz，并通过require('mz/fs');导入。mz提供的API和Node.js的fs模块完全相同，但fs模块使用回调，而mz封装了fs对应的函数，并改为Promise。这样，我们就可以非常简单的用await调用mz的函数，而不需要任何回调。
